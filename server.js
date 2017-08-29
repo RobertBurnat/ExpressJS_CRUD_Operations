dc29365d685fe7c55c997bc53a6523ff47fd451b
Robert Burnat <burnat.robertt@gmail.com>
2017-08-29 20:31:33 +0200

    Init

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var stringifyFile;
+var app = express();
+
+app.use(bodyParser.json());
+
+app.get('/getNote', function(req,res) {
+
+fs.readFile('./test.json', 'utf-8', function(err, data) {
+	if (err) throw err;
+	fileContent = data;
+	res.send(data);
+	});
+
+});
+
+app.post('/updateNote/:note', function(req,res) {
+	stringifyFile = req.params.note;
+	fs.appendFile('./test.json', stringifyFile, function(req, res, next) {
+		// if (err) throw err;
+		console.log('file updated');
+	});
+});
+
+app.listen(3000);
